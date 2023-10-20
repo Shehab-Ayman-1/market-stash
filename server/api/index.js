5e2469945fa3dc62852a905789e80b93ee7a6f43
Shehab Ayman <91635846+Shehab-Ayman-1@users.noreply.github.com>
2023-10-20 14:56:38 +0300

    try-vercel

--- server/api/index.js
+++
@@ -1,8 +0,0 @@
-import serverless from "serverless-http";
-import { app } from "../server.js";
-
-const handler = serverless(app);
-
-export default async (req, res) => {
-	await handler(req, res);
-};
