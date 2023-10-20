9ddb93e08429cbdefc18b5cb048b13f47f08b67f
Shehab Ayman <91635846+Shehab-Ayman-1@users.noreply.github.com>
2023-10-20 14:41:30 +0300

    try-vercel

--- vercel/index.js
+++ vercel/index.js
@@ -1,7 +1,9 @@
 import express from "express";
+import dotenv from "dotenv";
 
 const app = express();
 app.use(express.json());
+dotenv.config();
 
 app.get("/api/products", (req, res) => res.status(200).json({ url: req.url, method: req.method }));
 app.get("/api/users", (req, res) => res.status(200).json({ url: req.url, method: req.method }));
