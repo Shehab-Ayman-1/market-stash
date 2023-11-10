7677f31b9fb41d3760aac43b74d98ea860354c24
Shehab Ayman <91635846+Shehab-Ayman-1@users.noreply.github.com>
2023-11-10 19:51:41 +0200

    New_Commit

--- client/src/layout/ui/error.jsx
+++ client/src/layout/ui/error.jsx
@@ -2,17 +2,20 @@ import { useLocation } from "react-router-dom";
 import "./styles/error.scss";
 
 export const Error = ({ message }) => {
-	const { pathname, state } = useLocation();
+	const location = useLocation();
 	return (
 		<div className="error-section">
 			<div className="top">
 				<i className="fa fa-times-circle" />
 			</div>
-			<div className="bottom">
-				<h3>Oh My God!</h3>
+			<div className="bottom" dir="ltr">
+				<h3>Oh My God !</h3>
 				<p>{message}</p>
-				<p>{pathname}</p>
-				<p>{state}</p>
+				<div className="">
+					{Object.keys(location).map((key) => (
+						<p key={key}>{location[key] && `${key} : [ ${location[key]} ]`}</p>
+					))}
+				</div>
 			</div>
 		</div>
 	);
