43a7c5f89c96ba13a2084e3a4f8622023c2d394b
shehab ayman <shehabayman355@gmail.com>
2024-10-17 01:49:00 +0300

    deploy: update deployment

--- client/src/layout/navbar.jsx
+++ client/src/layout/navbar.jsx
@@ -15,6 +15,7 @@ export const Navbar = () => {
 			</Link>
 
 			<div className="icons">
+				<a href="https://sub-market-stash-client.vercel.app" className="fas fa-city gradient-text" />
 				<i className="fas fa-store-alt gradient-text" onClick={() => navigate("/bills")} />
 				<i className="fas fa-table gradient-text" onClick={() => navigate("/products")} />
 				<Dropdown icon="fa fa-bars gradient-text" windowClose linkClose>
